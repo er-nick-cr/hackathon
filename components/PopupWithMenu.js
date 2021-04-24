3b942fdaa0230c50cfb512af2ab1c4a40b977198
er-nick-cr <nikiteremeev2010@gmail.com>
2021-04-24 20:13:00 +0700

    feature: add popup menu js

---
+++ components/PopupWithMenu.js
@@ -0,0 +1,35 @@
+export default class PopupWithMenu {
+  constructor(popupElement, langElement) {
+    this._popupElement = document.querySelector(popupElement);
+    this._langElement = langElement;
+    this._handleClickOverlay = this._handleClickOverlay.bind(this);
+    this._menu = this._popupElement.querySelector(".menu");
+  }
+
+  open() {
+    this._popupElement.classList.add("navigation_opened");
+    this._langElement.classList.add("bio__page-language_visible");
+    console.log(this._menu);
+  }
+
+  close() {
+    this._popupElement.classList.remove("navigation_opened");
+    this._langElement.classList.remove("bio__page-language_visible");
+  }
+
+  _handleClickOverlay(evt) {
+    if (evt.target !== this._menu) {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popupElement.addEventListener(
+      "pointerdown",
+      this._handleClickOverlay
+    );
+    this._popupElement
+      .querySelector(".navigation__close-icon")
+      .addEventListener("click", () => this.close());
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import Popup from "../components/Popup.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithMenu from "../components/PopupWithMenu.js";
+import { popupImg, portfolioImage } from "../utils/constants.js";
+const popupWithImg = new PopupWithImage(popupImg);
+
+const menuButton = document.querySelector(".author__button");
+const menu = document.querySelector(".menu");
 const menuLinks = Array.from(document.querySelectorAll(".menu__link"));
+const langElement = document.querySelector(".bio__page-language");
 console.log(menuLinks);
 menuLinks.forEach((item) => {
   item.addEventListener("click", () => {
@@ -6,7 +15,14 @@ menuLinks.forEach((item) => {
     item.classList.add("menu__link_active");
   });
 });
-import PopupWithImage from "../components/PopupWithImage.js";
-import { popupImg, portfolioImage } from "../utils/constants.js";
-const popupWithImg = new PopupWithImage(popupImg);
-console(popupWithImg);
+
+const popupWithMenu = new PopupWithMenu(".navigation", langElement);
+
+menuButton.addEventListener("click", () => {
+  popupWithMenu.open();
+});
+popupWithMenu.setEventListeners();
+
+menu.addEventListener("pointerdown", (evt) => {
+  evt.stopPropagation();
+});
