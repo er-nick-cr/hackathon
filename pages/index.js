fa0501a8082021a97f264fbf26bd1822741ab823
Georgiy Gusev <gus3225@yandex.ru>
2021-04-24 20:48:37 +0500

    fix: удалить повторы

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
-=======
-import Popup from "../components/Popup.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithMenu from "../components/PopupWithMenu.js";
->>>>>>> a62e3e799951818e074e0b3a9399ac8f5501a6e1
 
 import {
   popupImg,
@@ -19,7 +14,6 @@ import {
   menu,
 } from "../utils/constants.js";
 
-<<<<<<< HEAD
 const popupWithImg = new PopupWithImage(popupImg)
 const popupWithSendMessage = new PopupWithForm({
   popupElement: popupSendMessage,
@@ -57,11 +51,6 @@ popupWithSendMessage.setEventListeners()
 popupSendMessageOpenButton.addEventListener('click', () => {
   popupWithSendMessage.open()
 })
-=======
-const popupWithImg = new PopupWithImage(popupImg);
-
-console.log(initialLinks.linkHowToLearn);
->>>>>>> a62e3e799951818e074e0b3a9399ac8f5501a6e1
 
 portfolioImageHowToLearn.addEventListener("click", () => {
   popupWithImg.open(initialLinks.linkHowToLearn);
