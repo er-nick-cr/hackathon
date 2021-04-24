1c80a607dd798fee8674c76df943e1101a17e24b
er-nick-cr <nikiteremeev2010@gmail.com>
2021-04-24 18:08:17 +0700

    feature: add links

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const menuLinks = Array.from(document.querySelectorAll(".menu__link"));
+console.log(menuLinks);
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach((item) => item.classList.remove("menu__link_active"));
+    item.classList.add("menu__link_active");
+  });
+});
